44f3f52c17475cbc7c0541a6d3f58f6046aaabf8
Cristi_1324 <crist@DESKTOP-R80PEDB>
2022-05-18 15:16:39 +0300

    rename

--- Proiect-FIS/src/Main.java
+++ Proiect-FIS/src/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class main extends JFrame {
+public class Main extends JFrame {
 
 	private JPanel contentPane;
 
@@ -16,7 +16,7 @@ public class main extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					main frame = new main();
+					Main frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -28,7 +28,7 @@ public class main extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public main() {
+	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
